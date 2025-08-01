cb67edf74770cadcb9184bc822a1b9f543b8da0a
vavlxxx <vladimir.nefedov357@gmail.com>
2025-08-01 21:37:33 +0500

    fix: tried to add rollback for connection lost ERROR

--- src/services/register.py
+++ src/services/register.py
@@ -6,17 +6,20 @@ from src.schemas.users import UserRequestDTO
 class RegisterService(BaseService):
 
     async def get_user_by(self, **fields):
+        await self.db.rollback()
         user = await self.db.users.get_one_or_none(**fields)
         if user is None:
             raise UserNotFoundException
         return user
     
     async def create_user(self, phone, telegram_id):
+        await self.db.rollback()
         user = UserRequestDTO(telegram_id=telegram_id, phone=phone)
         await self.db.users.add(user)
         self.db.commit()
     
     async def update_user(self, phone, telegram_id):
+        await self.db.rollback()
         user = UserRequestDTO(telegram_id=telegram_id, phone=phone)
         await self.db.users.edit(user, phone=phone)
         await self.db.commit()
